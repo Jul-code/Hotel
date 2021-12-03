3031a3d439b99cd702d9bda5a55048abff92dede
Jul-code <up02439@gmail.com>
2021-12-03 22:50:35 +0200

    Add Main

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import controller.ClientController;
+import model.HotelInfo;
+import view.HotelView;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HotelInfo model = new HotelInfo();
+        model.setPrice(1800);
+        HotelView view = new HotelView(model);
+        ClientController controller = new ClientController(model, view);
+
+        // Запуск программы/приложения
+    controller.runApp();
+    }
+}
+
