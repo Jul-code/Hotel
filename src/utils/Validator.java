b4141d3d6e0390f0acd292467e9ccc04e6966396
Jul-code <up02439@gmail.com>
2021-12-03 22:46:36 +0200

    Add Validator

---
+++ src/utils/Validator.java
@@ -0,0 +1,86 @@
+package utils;
+
+import java.util.Scanner;
+
+public class Validator {
+    // Валидация ввода имени
+    public static String validateName(Scanner scanner) {
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+            System.out.print("Пусто! Введите имя: ");
+            str = scanner.nextLine().trim();
+        }
+        return str;
+    }
+
+    //Валидация ввода названия отеля
+    public static String validateHotelName(Scanner scanner) {
+        String str = scanner.nextLine().trim();
+        while (str.isEmpty()) {
+            System.out.print("Пусто! Введите название отеля: ");
+            str = scanner.nextLine().trim();
+        }
+        return str;
+    }
+
+    // Валидация ввода количества ночей
+    public static int validateCostNightInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите количество ночей!: ");
+        }
+        int costNights = scanner.nextInt();
+        while (costNights <= 0) {
+            System.out.println("Неверное значение! Введите количество ночей: ");
+            while (!scanner.hasNextInt()) {
+                String str = scanner.next().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.println("Введите количество ночей!: ");
+            }
+            costNights = scanner.nextInt();
+        }
+        return costNights;
+    }
+
+    // Валидация ввода количества посетителей
+    public static int validateCostVisitorsInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите количество посетителей!: ");
+        }
+        int costVisitors = scanner.nextInt();
+        while (costVisitors <= 0) {
+            System.out.print("Неверное значение! Введите количество посетителей: ");
+            while (!scanner.hasNextInt()) {
+                String str = scanner.nextLine().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.print("Введите количество посетителей!: ");
+            }
+            costVisitors = scanner.nextInt();
+        }
+        return costVisitors;
+    }
+
+    // Валидация ввода количества звезд отеля
+    public static int validateStarsInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            String str = scanner.nextLine().trim();
+            System.out.printf("\"%s\" - не число!\n", str);
+            System.out.print("Введите количество звезд отеля!: ");
+        }
+        int stars = scanner.nextInt();
+        while (stars <= 0) {
+            System.out.print("Неверное значение! Введите количество звезд отеля!: ");
+            while (!scanner.hasNextInt()) {
+                String str = scanner.nextLine().trim();
+                System.out.printf("\"%s\" - не число!\n", str);
+                System.out.print("Введите количество звезд отеля!: ");
+            }
+            stars = scanner.nextInt();
+        }
+        return stars;
+    }
+}
+
