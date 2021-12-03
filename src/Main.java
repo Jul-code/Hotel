3129e422296adf6c4e7aa222949c1afe478cd786
Jul-code <up02439@gmail.com>
2021-12-04 00:41:54 +0200

    Change price

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         HotelInfo model = new HotelInfo();
-        model.setPrice(1800);
+        model.setPrice(1000);
         HotelView view = new HotelView(model);
         ClientController controller = new ClientController(model, view);
 
