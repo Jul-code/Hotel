449dfd5d7028b696a6a9097577ab2e409aeea4e0
Jul-code <up02439@gmail.com>
2021-12-03 22:44:36 +0200

    Add model for HotelInfo

---
+++ src/model/HotelInfo.java
@@ -0,0 +1,74 @@
+package model;
+
+import base.Nights;
+import base.Stars;
+import base.Visitors;
+
+public class HotelInfo implements Nights, Visitors, Stars {
+
+    // Model.
+// Применение интерфейсов.
+    private String name;
+    private String hotelName;
+    private int nights;
+    private int visitors;
+    private int stars;
+    private double price;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getStars() { return stars; }
+
+    public void setStars(int stars) { this.stars = stars; }
+
+    public int getVisitors() {
+        return visitors;
+    }
+
+    public void setVisitors(int visitors) {
+        this.visitors = visitors;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public void setNights(int nights) {
+        this.nights = nights;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public double costNumbersOfNights(int nights, double price) {
+        return nights * price;
+    }
+
+    @Override
+    public double costWithVisitors(int visitors, double cost) {
+        return visitors * cost;
+    }
+
+    @Override
+    public double applyStars(int stars, double cost) { return stars * cost; }
+}
